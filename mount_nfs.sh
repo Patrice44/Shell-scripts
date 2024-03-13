3dc646ee6a01d1391636757a917bfbca35641d52
Patrice44 <50078400+Patrice44@users.noreply.github.com>
2024-03-13 10:49:56 +0100

    Update mount_nfs.sh
    Anonymise l'add IP

--- mount_nfs.sh
+++ mount_nfs.sh
@@ -1,19 +1,19 @@
 #/bin/bash
-echo "###  sudo mount -rw -o vers=4 192.168.1.204:/volume1/homes/patrice /nfs/DS916_home"
+echo "###  sudo mount -rw -o vers=4 192.168.1.xxx:/volume1/homes/patrice /nfs/DS916_home"
 echo ""
-sudo mount -rw -o vers=4 192.168.1.204:/volume1/homes/patrice /nfs/DS916_home
+sudo mount -rw -o vers=4 192.168.1.xxx:/volume1/homes/patrice /nfs/DS916_home
 echo ""
-echo "###  mount -rw -o vers=4 192.168.1.204:/volume1/Public /nfs/DS916_public"
+echo "###  mount -rw -o vers=4 192.168.1.xxx:/volume1/Public /nfs/DS916_public"
 echo ""
-sudo mount -rw -o vers=4 192.168.1.204:/volume1/Public /nfs/DS916_public
+sudo mount -rw -o vers=4 192.168.1.xxx:/volume1/Public /nfs/DS916_public
 echo ""
-echo "###  mount -rw -o vers=4 192.168.1.204:/volume1/music /nfs/DS916_music"
+echo "###  mount -rw -o vers=4 192.168.1.xxx:/volume1/music /nfs/DS916_music"
 echo ""
-sudo mount -rw -o vers=4 192.168.1.204:/volume1/music /nfs/DS916_music
+sudo mount -rw -o vers=4 192.168.1.xxx:/volume1/music /nfs/DS916_music
 #
 echo "###  Liste des point de montage du Synology DS916+"
 echo ""
-sudo showmount -e 192.168.1.204
+sudo showmount -e 192.168.1.xxx
 #
 echo ""
 echo "###  sudo mount |grep nfs"
